367997b582af959206184734b1039d35642b59a7
no_echo <2215853783@qq.com>
2024-08-01 17:15:01 +0800

    add the file of nn_seq

---
+++ learn_pytorch/scr/P22_nn_seq.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+from torch.nn import Sequential, Conv2d, MaxPool2d, Flatten, Linear
+from torch.utils.tensorboard import SummaryWriter
+
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.model1 = Sequential(
+            Conv2d(3, 32, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 32, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+        )
+    def forward(self, x):
+        x = self.model1(x)
+        return x
+
+tudui = Tudui()
+print(tudui)
+input = torch.ones((64, 3, 32, 32))
+output = tudui(input)
+print(output.shape)
+
+Writer = SummaryWriter(log_dir='../logs/logs_seq')
+Writer.add_graph(tudui, input)
+Writer.close()
