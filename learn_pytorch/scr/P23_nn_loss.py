31f817ab928470a237103bfaa30c6f8d1b5dc48f
no_echo <2215853783@qq.com>
2024-08-02 11:21:36 +0800

    write the file of nn_loss

--- learn_pytorch/scr/P23_nn_loss.py
+++ learn_pytorch/scr/P23_nn_loss.py
@@ -1,5 +1,6 @@
 import torch
-from torch.nn import L1Loss
+from torch import nn
+from torch.nn import L1Loss, MSELoss
 
 inputs = torch.tensor([1, 2, 3], dtype=torch.float)
 targets = torch.tensor([1, 2, 5], dtype=torch.float)
@@ -10,4 +11,15 @@ targets = torch.reshape(targets, (1, 1, 1, 3))
 loss = L1Loss(reduction="sum")
 result = loss(inputs, targets)
 
+loss_mse = MSELoss()
+result_mse = loss_mse(inputs, targets)
+
 print(result)
+print(result_mse)
+
+x = torch.tensor([0.1, 0.2, 0.3])
+y = torch.tensor([1])
+x = torch.reshape(x, (1, 3))
+loss_cross = nn.CrossEntropyLoss()
+result_cross = loss_cross(x, y)
+print(result_cross)
