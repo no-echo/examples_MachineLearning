b139b2f22726fa8a90f1b390fb9e02daedd50899
no_echo <2215853783@qq.com>
2024-08-06 16:15:52 +0800

    add the file of P27_model.py and P27_train.py

---
+++ learn_pytorch/scr/P27_train.py
@@ -0,0 +1,75 @@
+import torch
+import torchvision
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from P27_model import *
+# 准备数据集
+train_data = torchvision.datasets.CIFAR10(root='../data/data_18', train=True, download=True,
+                                          transform=torchvision.transforms.ToTensor())
+test_data = torchvision.datasets.CIFAR10(root='../data/data_18', train=False,download=True,
+                                         transform=torchvision.transforms.ToTensor())
+
+# length长度
+train_data_size = len(train_data)
+test_data_size = len(test_data)
+print("训练数据集的长度为:{}".format(train_data_size))
+print("测试数据集的长度为:{}".format(test_data_size))
+
+# 利用dataloader来加载数据集
+train_dataloader = DataLoader(train_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+# 创建网络模型
+tudui = Tudui()
+
+# 损失函数
+loss_fn = nn.CrossEntropyLoss()
+
+# 优化器
+learning_rate = 0.01
+optimizer = torch.optim.SGD(tudui.parameters(), lr=learning_rate)
+
+# 设置训练网络的一些参数
+# 记录训练的次数
+total_train_step = 0
+# 记录测试的次数
+total_test_step = 0
+# 训练的论数
+epoch = 10
+
+# 添加tensorboard
+writer = SummaryWriter(log_dir='../logs/log_train')
+
+for i in range(epoch):
+    print("--------第{}轮训练开始了-----------".format(i+1))
+
+    # 训练步骤开始
+    for data in train_dataloader:
+        imgs, targets = data
+        outputs = tudui(imgs)
+        loss = loss_fn(outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_train_step += 1
+        if total_train_step % 100 == 0:
+            print("训练次数：{},Loss:{}".format(total_train_step, loss.item()))
+            writer.add_scalar('train_loss', loss.item(), total_train_step)
+
+    # 测试步骤开始
+    total_test_loss = 0
+    with torch.no_grad():
+        for data in test_dataloader:
+            imgs, targets = data
+            outputs = tudui(imgs)
+            loss = loss_fn(outputs, targets)
+            total_test_loss += total_test_loss + loss.item()
+    print("整体测试集上的Loss: {}".format(total_test_loss))
+    writer.add_scalar('test_loss', total_test_loss, total_test_step)
+    total_test_step += 1
+
+writer.close()
